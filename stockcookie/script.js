f2a3ef69f16fe7a8e35869f54e3210faa1695aab
mrznyper <mf.mrznyper@gmail.com>
2015-11-21 19:58:22 -0500

    Stock Exists test.
    Trying cookies out.

--- stockcookie/script.js
+++ stockcookie/script.js
@@ -1,35 +1,52 @@
 var welcome = `<div class="demo-card-wide mdl-shadow--2dp">
-	<div class="mdl-card__title">
-	<h2 class="mdl-card__title-text mdl-color-text--primary-dark">You don't have a cookie!</h2>
-	</div>
-	<div class="mdl-card__supporting-text">
-	<strong>That's ok!</strong><br>
-	Simply enter the stock tickers to you want to follow.
-	</div>
-	<div class="mdl-card__actions mdl-card--border">
-		<form action="bakeCookie">
-		  <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-			<input class="mdl-textfield__input" type="text" id="tickerInput">
-			<label class="mdl-textfield__label" for="tickerInput">TWTR, MSFT, AAPL, SNE...</label>
-		  </div>
-		  <button id="bakeCookieButton" class="button_cookie mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect">
-		</button>
-		</form>
-		<div class="mdl-layout-spacer"></div>
-	</div>
-	</div>
-</div>`;
+					<div class="mdl-card__title">
+					<h2 class="mdl-card__title-text mdl-color-text--primary-dark">You don't have a cookie!</h2>
+					</div>
+					<div class="mdl-card__supporting-text">
+					<strong>That's ok!</strong><br>
+					Simply enter the stock tickers to you want to follow.
+					</div>
+					<div class="mdl-card__actions mdl-card--border">
+						<form>
+							<div class="mdl-textfield mdl-js-textfield">
+								<input class="mdl-textfield__input mdl-color-text--primary-dark" type="text" id="tickerInput">GOOG, MSFT, AAPL, SNE...</input>
+							</div>
+							<button onclick="bakeCookie()" id="bakeCookieButton" class="button_cookie mdl-button mdl-js-button mdl-button--fab"/>
+						</form>
+					</div>
+					</div>
+				</div>`;
 
+var stock_card = `<div class="mdl-cell mdl-cell--3-col mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--3dp">
+					  <div class="mdl-card__title">
+						 <h4 class="mdl-card__title-text">[STOCK_TITLE]</h4>
+					  </div>
+					  <div class="mdl-card__supporting-text">
+						<span class="mdl-typography--font-light mdl-typography--subhead">[STOCK_TEXT]</span>
+					  </div>
+					  <div class="mdl-card__actions">
+						 <a class="android-link mdl-button mdl-js-button mdl-typography--text-uppercase" href="" data-upgraded=",MaterialButton">
+						   [STOCK_LINK]
+						   <i class="material-icons">chevron_right</i>
+						 </a>
+					  </div>
+					</div>`;
+					
+var stock_div = `<div class="android-card-container mdl-grid">
+				[STOCK_CARDS]	
+				</div>`;
+				
 window.onload = function() {
   checkCookie();
 };
 function checkCookie(){
-	//var cookie = readCookie("stocks");
-	//if(cookie = null){
-		document.getElementById('mainContainer').innerHTML= welcome;
-	//}else{
-		//populateStocks();
-	//}
+	var cookie = readCookie("stocks");
+	alert(cookie);
+	if(cookie == null){
+		document.getElementById('mainContainer').innerHTML = welcome;
+	}else{
+		document.getElementById('mainContainer').innerHTML = populateStocks(cookie);
+	}
 }
 
 //Found http://www.quirksmode.org/js/cookies.html
@@ -56,4 +73,14 @@ function readCookie(name) {
 
 function eraseCookie(name) {
 	createCookie(name,"",-1);
+}
+
+function bakeCookie(){
+	var raw_tickers = document.getElementById("tickerInput").value;
+	alert(raw_tickers);
+	createCookie("stocks", raw_ticker, 5);
+}
+
+function populateStocks(cookie){
+	return cookie;
 }
